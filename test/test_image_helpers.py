ac8f7c61b471089d2895ee12ace5407469d1ed5c
Fabien LOISON <flo@flogisoft.com>
2017-12-14 11:46:24 +0100

    image helpers

---
+++ test/test_image_helpers.py
@@ -0,0 +1,40 @@
+import pytest
+from PIL import Image
+
+from yoga.image import helpers
+
+
+class TestImageHaveAlpha(object):
+
+    def test_jpeg_without_alpha(self):
+        image = Image.open("test/images/image1.jpg")
+        assert not helpers.image_have_alpha(image)
+
+    def test_png_without_alpha(self):
+        image = Image.open("test/images/unused-alpha.png")
+        assert not helpers.image_have_alpha(image)
+
+    def test_png_with_alpha(self):
+        image = Image.open("test/images/alpha.png")
+        assert helpers.image_have_alpha(image)
+
+    def test_threshold(self):
+        image = Image.open("test/images/threshold.png")
+        assert helpers.image_have_alpha(image, 0xEF)
+        assert not helpers.image_have_alpha(image, 0xE0)
+
+
+class TestGessImageFormat(object):
+
+    def test_jpeg_image(self):
+        image_bytes = open("test/images/image1.jpg", "rb").read()
+        assert helpers.guess_image_format(image_bytes) == "jpeg"
+
+    def test_png_image(self):
+        image_bytes = open("test/images/alpha.png", "rb").read()
+        assert helpers.guess_image_format(image_bytes) == "png"
+
+    def test_unsuported_image_format(self):
+        image_bytes = open("test/images/alpha.svg", "rb").read()
+        with pytest.raises(ValueError):
+            helpers.guess_image_format(image_bytes)
---
+++ yoga/image/helpers.py
@@ -0,0 +1,19 @@
+def image_have_alpha(image, threshold=0xFE):
+    if threshold <= 0:
+        return False
+    if image.mode != "RGBA":
+        return False
+    alpha_band = image.getdata(band=3)  # bands: R=0, G=1, B=2, A=3
+    for pix in alpha_band:
+        if pix <= threshold:
+            return True
+    return False
+
+
+def guess_image_format(image_initial_bytes):
+    if image_initial_bytes.startswith(b"\xFF\xD8\xFF\xE0"):
+        return "jpeg"
+    elif image_initial_bytes.startswith(b"\x89PNG\r\n"):
+        return "png"
+    else:
+        raise ValueError("Unsupported image format")
