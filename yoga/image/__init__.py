aefa6219c8b3c6d83e4b337a6cbfe64bcf9182e2
Fabien LOISON <flo@flogisoft.com>
2017-12-22 16:22:04 +0100

    wip image.optimize()

--- yoga/image/__init__.py
+++ yoga/image/__init__.py
@@ -1,8 +1,34 @@
+from PIL import Image
+
 from .options import normalize_options
 
 
 def optimize(input_file, output_file, options={}):
     options = normalize_options(options)
-    # TODO input_file -> string (path), bytes, file-like
-    # TODO output_file -> string (path), file-like
-    raise NotImplementedError()  # TODO
+
+    input_image = Image.open(input_file)
+
+    if options["output_format"] == "orig" and input_image.format not in ("JPEG", "PNG"):                      # noqa
+        raise ValueError("The input image must be a JPEG or a PNG when setting 'output_format' to 'orig'")    # noqa
+
+    if not hasattr(output_file, "write"):
+        output_file = open(output_file, "wb")
+
+    # resize
+    # TODO
+
+    # output format
+    output_format = None
+
+    if options["output_format"] == "orig":
+        output_format = input_image.format.lower()
+    elif options["output_format"] in ("jpeg", "png"):
+        output_format = options["output_format"]
+    else:  # auto
+        raise NotImplementedError()
+
+    # convert / optimize
+    # TODO
+
+    # write to output_file
+    input_image.save(output_file, format=output_format)  # FIXME
