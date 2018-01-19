5f1fe80edc24ab2498ebd585fb3f4fadf89769a6
Alexis Breust <abreust@wanadev.fr>
2018-01-19 11:15:43 +0100

    Added c++11 flag during build

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ from setuptools.command.build_py import build_py
 class CustomBuildPy(build_py):
 
     def run(self):
+        os.environ["CPPFLAGS"] = "--std=c++11"
         subprocess.call("cd assimp/ && mkdir -p build && cd build && \
                          cmake .. -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_TESTS=OFF && \
                          make", shell=True)
