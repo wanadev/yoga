bb3d2de51f509ca6015a22a6c5202342d872af27
Fabien LOISON <flo@flogisoft.com>
2018-03-02 17:32:05 +0100

    parametrize test_image_helpers.py

--- test/test_image_helpers.py
+++ test/test_image_helpers.py
@@ -6,41 +6,41 @@ from yoga.image import helpers
 
 class Test_image_have_alpha(object):
 
-    def test_jpeg_without_alpha(self):
-        image = Image.open("test/images/image1.jpg")
+    @pytest.mark.parametrize("image_path", [
+        "test/images/image1.jpg",
+        "test/images/unused-alpha.png",
+        "test/images/indexed.png",
+        "test/images/grayscale.png",
+        ])
+    def test_image_without_alpha(self, image_path):
+        image = Image.open(image_path)
         assert not helpers.image_have_alpha(image)
 
-    def test_png_without_alpha(self):
-        image = Image.open("test/images/unused-alpha.png")
-        assert not helpers.image_have_alpha(image)
-
-    def test_png_with_alpha(self):
+    def test_image_with_alpha(self):
         image = Image.open("test/images/alpha.png")
         assert helpers.image_have_alpha(image)
 
-    def test_threshold(self):
+    @pytest.mark.parametrize("image_path, threshold, is_alpha", [
+        ("test/images/threshold.png", 0xEF, True),
+        ("test/images/threshold.png", 0xE0, False),
+        ])
+    def test_alpha_threshold(self, image_path, threshold, is_alpha):
         image = Image.open("test/images/threshold.png")
-        assert helpers.image_have_alpha(image, 0xEF)
-        assert not helpers.image_have_alpha(image, 0xE0)
-
-    def test_indexed_png(self):
-        image = Image.open("test/images/indexed.png")
-        assert not helpers.image_have_alpha(image)
-
-    def test_grayscale_png(self):
-        image = Image.open("test/images/grayscale.png")
-        assert not helpers.image_have_alpha(image)
+        if is_alpha:
+            assert helpers.image_have_alpha(image, threshold)
+        else:
+            assert not helpers.image_have_alpha(image, threshold)
 
 
 class Test_gess_image_format(object):
 
-    def test_jpeg_image(self):
-        image_bytes = open("test/images/image1.jpg", "rb").read()
-        assert helpers.guess_image_format(image_bytes) == "jpeg"
-
-    def test_png_image(self):
-        image_bytes = open("test/images/alpha.png", "rb").read()
-        assert helpers.guess_image_format(image_bytes) == "png"
+    @pytest.mark.parametrize("image_path, expected_format", [
+        ("test/images/image1.jpg", "jpeg"),
+        ("test/images/alpha.png", "png"),
+        ])
+    def test_supported_image_format(self, image_path, expected_format):
+        image_bytes = open(image_path, "rb").read()
+        assert helpers.guess_image_format(image_bytes) == expected_format
 
     def test_unsuported_image_format(self):
         image_bytes = open("test/images/alpha.svg", "rb").read()
