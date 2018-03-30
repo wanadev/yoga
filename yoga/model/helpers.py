21a761f71e0ef38c567eef0208a455e6489b48a0
Alexis Breust <abreust@wanadev.fr>
2018-03-30 09:31:52 +0200

    Fixed duplicated textures by yoga

--- yoga/model/helpers.py
+++ yoga/model/helpers.py
@@ -45,6 +45,8 @@ def find_valid_path(path, root_path):
 
 def model_embed_images(images, images_bytes,
                        optimize_textures, root_path, image_options):
+    optimized_images = {}
+
     image = images
     while image:
         if image.bytes_length > 0:
@@ -52,6 +54,16 @@ def model_embed_images(images, images_bytes,
 
         image_path = ffi.string(image.path).decode("utf-8")
         valid_image_path = find_valid_path(image_path, root_path)
+        valid_image_path = os.path.abspath(valid_image_path)
+
+        # If valid_image_path have already been seen, do not reoptimize...
+        if valid_image_path in optimized_images:
+            optimized_image = optimized_images[valid_image_path]
+            image.bytes_length = optimized_image.bytes_length
+            image.bytes = optimized_image.bytes
+            image.id = optimized_image.id
+            image = image.next
+            continue
 
         # Optimizing images
         image_io = io.BytesIO(open(valid_image_path, "rb").read())
@@ -68,6 +80,9 @@ def model_embed_images(images, images_bytes,
         image_bytes_c = ffi.new("char[%d]" % len(image_bytes), image_bytes)
         image.bytes_length = len(image_bytes)
         image.bytes = image_bytes_c
+        image.id = len(optimized_images)
+
+        optimized_images[valid_image_path] = image
         image = image.next
 
         # @note Save the bytes to a dictionnary so that the garbage collector
