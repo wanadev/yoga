9171c61564657aef9071099f55affcd3164e0e5b
Fabien LOISON <flo@flogisoft.com>
2020-05-06 17:19:01 +0200

    Workaround a Nuitka issue with Windows icons...

--- winbuild/builddist.bat
+++ winbuild/builddist.bat
@@ -6,10 +6,18 @@ call __env__\Scripts\activate.bat
 pip install .
 pip install nuitka
 
-python -m nuitka --follow-imports --standalone --windows-dependency-tool=pefile --windows-icon=winbuild\yoga-icon.ico winbuild\yogawin.py
+REM Hack to workaround an issue with the way Nuitka handles the icons...
+mkdir yogawin.build
+copy winbuild\yoga-icon.ico yogawin.build\yoga-icon.ico
+copy winbuild\yoga-icon.ico yoga-icon.ico
+
+python -m nuitka --follow-imports --standalone --windows-dependency-tool=pefile --windows-icon=yoga-icon.ico winbuild\yogawin.py
 move yogawin.dist\yogawin.exe yogawin.dist\yoga.exe
 copy winbuild\README-windows-dist.md yogawin.dist\README.txt
 copy LICENSE yogawin.dist\LICENSE.txt
+
+REM Remove the icon that was put here to workaround Nuika icon issue...
+del yoga-icon.ico
 
 call __env__\Scripts\deactivate.bat
 
