b7acc02233ee1f37fc178c05799883baa060f0ac
Fabien LOISON <flo@flogisoft.com>
2020-05-06 15:55:09 +0200

    Adds an icon for Windows

--- winbuild/builddist.bat
+++ winbuild/builddist.bat
@@ -6,7 +6,7 @@ call __env__\Scripts\activate.bat
 pip install .
 pip install nuitka
 
-python -m nuitka --follow-imports --standalone --windows-dependency-tool=pefile winbuild\yogawin.py
+python -m nuitka --follow-imports --standalone --windows-dependency-tool=pefile --windows-icon=winbuild\yoga-icon.ico winbuild\yogawin.py
 move yogawin.dist\yogawin.exe yogawin.dist\yoga.exe
 copy winbuild\README-windows-dist.md yogawin.dist\README.txt
 copy LICENSE yogawin.dist\LICENSE.txt
