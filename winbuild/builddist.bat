1b7b14b83dfbe1ec1880622bec114faec19c4651
Fabien LOISON <flo@flogisoft.com>
2020-05-06 15:33:19 +0200

    Update windows build script and add a README and a license to the dist dir

--- winbuild/builddist.bat
+++ winbuild/builddist.bat
@@ -7,6 +7,9 @@ pip install .
 pip install nuitka
 
 python -m nuitka --follow-imports --standalone --windows-dependency-tool=pefile winbuild\yogawin.py
+move yogawin.dist\yogawin.exe yogawin.dist\yoga.exe
+copy winbuild\README-windows-dist.md yogawin.dist\README.txt
+copy LICENSE yogawin.dist\LICENSE.txt
 
 call __env__\Scripts\deactivate.bat
 
