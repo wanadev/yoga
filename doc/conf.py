b7b78e1ac2ccb609db5cfb7f893c8865a8f079cd
Fabien LOISON <flo@flogisoft.com>
2021-01-19 10:37:02 +0100

    Shpinx: add the githubpages extention to create the .nojekyll file

--- doc/conf.py
+++ doc/conf.py
@@ -28,6 +28,7 @@ author = 'Wanadev'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
