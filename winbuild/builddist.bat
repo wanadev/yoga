bf1c40e51f6c49491ab08cf2ca929fcad0e4f5db
Fabien LOISON <flo@flogisoft.com>
2025-01-08 14:53:56 +0100

    misc: Removed an old hack from the Windows standalone build script

--- winbuild/builddist.bat
+++ winbuild/builddist.bat
@@ -6,25 +6,17 @@ call __env__\Scripts\activate.bat
 pip install .
 pip install -r winbuild\requirements.txt
 
-REM Hack to workaround an issue with the way Nuitka handles the icons...
-mkdir yogawin.build
-copy winbuild\yoga-icon.ico yogawin.build\yoga-icon.ico
-copy winbuild\yoga-icon.ico yoga-icon.ico
-
 python -m nuitka ^
     --standalone ^
     --follow-imports ^
     --assume-yes-for-downloads ^
     --include-package=PIL ^
     --include-package=_cffi_backend ^
-    --windows-icon-from-ico=yoga-icon.ico ^
+    --windows-icon-from-ico=winbuild\yoga-icon.ico ^
     winbuild\yogawin.py
 move yogawin.dist\yogawin.exe yogawin.dist\yoga.exe
 copy winbuild\README-windows-dist.md yogawin.dist\README.txt
 copy LICENSE yogawin.dist\LICENSE.txt
-
-REM Remove the icon that was put here to workaround Nuika icon issue...
-del yoga-icon.ico
 
 endlocal
 
